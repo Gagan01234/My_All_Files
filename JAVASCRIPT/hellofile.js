f0f57d033218658ce48e03fd50dcab07bc0ad497
gagan <gagangowda@gmail.com>
2022-12-08 21:59:21 +0530

    Commiting my all Web development files

---
+++ JAVASCRIPT/hellofile.js
@@ -0,0 +1,8 @@
+
+document.write("External file");//My first program
+document.write("<h2>Gagan gowda vs</h2>");
+/* This 
+is 
+multi-line
+comment*/
+//https://youtube.com/playlist?list=PLsyeobzWxl7qtP8Lo9TReqUMkiOp446cV
---
+++ JAVASCRIPT/loadunload.js
@@ -0,0 +1,8 @@
+function greeting()
+{
+    document.write("Welcome to my webpage");
+}
+function goodbye()
+{
+    document.write("Thank you, visit again");
+}
